adb4043257dacd1020b4003436114eb482e3d157
Shweta Chavan <chavan.shweta@gmail.com>
2022-03-14 12:50:08 -0400

    Update 1_run_fastp_multiqc.sh

--- 1_run_fastp_multiqc.sh
+++ 1_run_fastp_multiqc.sh
@@ -9,8 +9,8 @@ SAMPLESHEET1="../inputs/samplesheet_EXP21001376.tsv"
 mkdir -p $QCFOLDER
 while read -r SAMPLE FASTQ1 FASTQ2 REST; do
     echo "Working on --> "$SAMPLE
-    fastp -h outputsQC/${SAMPLE}_fastp.html -j outputsQC/${SAMPLE}_fastp.json -i ${FASTQ1} -I ${FASTQ2} -o ${FASTQ1}.trimmed.fastq.gz -O ${FASTQ2}.trimmed.fastq.gz --dedup --cut_front --cut_tail_window_size --cut_tail --cut_front_window_size --n_base_limit --length_required 40 --low_complexity_filter --overrepresentation_analysis
-
+    fastp -h outputsQC/${SAMPLE}_fastp.html -j outputsQC/${SAMPLE}_fastp.json -i ${FASTQ1} -I ${FASTQ2} -o ${FASTQ1}.trimmed.fastq.gz -O ${FASTQ2}.trimmed.fastq.gz \
+    -y -3 --cut_tail_window_size 4 -5 --cut_front_window_size 4 -l 40 -n 5 -p
     fastqc ${FASTQ1} ${FASTQ2}
 done < $SAMPLESHEET1
 
